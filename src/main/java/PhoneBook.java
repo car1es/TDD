78d6adc59c5e44390eef4e507f97bda621720e3c
Alexey Gorokhov <car1es@bk.ru>
2023-02-01 11:49:32 +0300

    Stub and tests for printAllNames

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -6,7 +6,7 @@ public class PhoneBook {
     private Map<String, String> map = new HashMap<>();
 
     public boolean add(String name, String number) {
-        if (!map.containsKey(name)){
+        if (!map.containsKey(name)) {
             map.put(name, number);
             System.out.println("Количество контактов в телефонной книке " + map.size());
             return true;
@@ -14,16 +14,21 @@ public class PhoneBook {
         System.out.println("Такое имя уже есть в телефонной книге, размер книги остался прежним: " + map.size());
         return false;
     }
-    public String findByName(String name){
+
+    public String findByName(String name) {
         return map.get(name);
     }
 
-    public String findByNumber(String number){
+    public String findByNumber(String number) {
         for (String key : map.keySet()) {
             if (map.get(key).equals(number)) {
                 return key;
             }
         }
+        return null;
+    }
+
+    public Set<String> printAllNames() {
         return null;
     }
 
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,5 +1,10 @@
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class PhoneBookTest {
     @Test
@@ -19,21 +24,38 @@ public class PhoneBookTest {
         boolean result = phoneBook.add("Vasya", "+79000010110");
         Assertions.assertEquals(expected, result);
     }
+
     @Test
-    public void findByNameTest(){
+    public void findByNameTest() {
         PhoneBook phoneBook = new PhoneBook();
         phoneBook.add("Vasya", "+79000010110");
         final String expected = "+79000010110";
         String result = phoneBook.findByName("Vasya");
-        Assertions.assertEquals(expected,result);
+        Assertions.assertEquals(expected, result);
     }
+
     @Test
-    public void findByNumberTest(){
+    public void findByNumberTest() {
         PhoneBook phoneBook = new PhoneBook();
         phoneBook.add("Vasya", "+79000010110");
         final String expected = "Vasya";
         String result = phoneBook.findByNumber("+79000010110");
-        Assertions.assertEquals(expected,result);
+        Assertions.assertEquals(expected, result);
     }
+
+    @Test
+    public void printAllNamesTest() {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Borya", "2");
+        phoneBook.add("Petya", "3");
+        phoneBook.add("Denis", "4");
+        phoneBook.add("Anya", "1");
+
+        Set<String> result = phoneBook.printAllNames();
+        String[] expected = {"Anya", "Borya", "Denis", "Petya"};
+
+        assertThat(result.toArray(), Matchers.equalTo(expected));
+    }
+
 
 }
